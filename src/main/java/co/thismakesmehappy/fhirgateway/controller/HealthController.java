20cbb5b6c091f7dba90b3be5af74fad4529ad6a8
Bernardo Margulis <bernardo@thismakesmehappy.co>
2026-05-04 16:55:33 -0400

    Add HealthController with GET /healthz

---
+++ src/main/java/co/thismakesmehappy/fhirgateway/controller/HealthController.java
@@ -0,0 +1,15 @@
+package co.thismakesmehappy.fhirgateway.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class HealthController {
+    @GetMapping("/healthz")
+    public ResponseEntity<Map<String, String>> health() {
+        return ResponseEntity.ok(Map.of("status", "ok"));
+    }
+}
