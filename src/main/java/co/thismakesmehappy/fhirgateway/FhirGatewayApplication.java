8a652c1e27e76772f2dcfac9ee4f04f54e4f2413
Bernardo Margulis <bernardo@thismakesmehappy.co>
2026-05-04 16:37:01 -0400

    Initial project scaffold from Spring Initializr

---
+++ src/main/java/co/thismakesmehappy/fhirgateway/FhirGatewayApplication.java
@@ -0,0 +1,13 @@
+package co.thismakesmehappy.fhirgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FhirGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FhirGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/thismakesmehappy/fhirgateway/FhirGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package co.thismakesmehappy.fhirgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FhirGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
